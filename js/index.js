7eabd821c6d27f84ef80f735479a5447ecff95d6
@kristina.rud5 <kristina.rud5@gmail.com>
2022-12-25 23:55:01 +0200

    added code load work section card

--- js/index.js
+++ js/index.js
@@ -1,24 +1,359 @@
 "use strict";
 
-const tabsTitle = document.querySelectorAll(".tabs-title");
-const tabsContent = document.querySelectorAll(".tab");
-let activeTab;
+const tabsTitle = document.querySelectorAll(".tabs-title"),
+  tabsContent = document.querySelectorAll(".tab"),
+  tabsList = document.querySelector(".tabs"),
+  activeTabClass = "active";
 
-function selectTabTitle() {
-  tabsTitle.forEach((item) => item.classList.remove("active"));
-  this.classList.add("active");
-  activeTab = this.dataset.tabName;
-  selectTabContent(activeTab);
+function selectTitle(element, titlesList, className) {
+  titlesList.forEach((item) => item.classList.remove(className));
+  element.classList.add(className);
 }
 
-function selectTabContent(activeTab) {
-  tabsContent.forEach((item) =>
-    item.classList.contains(activeTab)
-      ? item.classList.add("active")
-      : item.classList.remove("active")
+function selectContent(activeTab, contentList, className) {
+  contentList.forEach((item) =>
+    item.dataset.category === activeTab
+      ? item.classList.add(className)
+      : item.classList.remove(className)
   );
 }
+tabsList.addEventListener("click", (element) => {
+  const target = element.target.closest("li");
+  selectTitle(target, tabsContent);
+  selectContent(target.dataset.category, tabsContent, activeTabClass);
+});
 
-tabsTitle.forEach((item) => {
-  item.addEventListener("click", selectTabTitle);
+// second part
+
+const imagesArray = [
+  {
+    id: 1,
+    imgLink: "./img/services/graphic-design1.jpg",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 2,
+    imgLink: "./img/all/Layer 24.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 3,
+    imgLink: "./img/all/Layer 25.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 4,
+    imgLink: "./img/all/Layer 26.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 5,
+    imgLink: "./img/all/Layer 27.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 6,
+    imgLink: "./img/all/Layer 28.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 7,
+    imgLink: "./img/all/Layer 29.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 8,
+    imgLink: "./img/all/Layer 30.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 9,
+    imgLink: "./img/all/Layer 31.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 10,
+    imgLink: ".//img/all/Layer 32.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 11,
+    imgLink: "./img/all/Layer 33.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 12,
+    imgLink: "./img/all/Layer 34.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 13,
+    imgLink: "./img/services/graphic-design1.jpg",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 14,
+    imgLink: "./img/all/Layer 24.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 15,
+    imgLink: "./img/all/Layer 25.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 16,
+    imgLink: "./img/all/Layer 26.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 17,
+    imgLink: "./img/all/Layer 27.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 18,
+    imgLink: "./img/all/Layer 28.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 19,
+    imgLink: "./img/all/Layer 29.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 20,
+    imgLink: "./img/all/Layer 30.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 21,
+    imgLink: "./img/all/Layer 31.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 22,
+    imgLink: ".//img/all/Layer 32.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 23,
+    imgLink: "./img/all/Layer 33.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 24,
+    imgLink: "./img/all/Layer 34.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 25,
+    imgLink: "./img/services/graphic-design1.jpg",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 26,
+    imgLink: "./img/all/Layer 24.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 27,
+    imgLink: "./img/all/Layer 25.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 28,
+    imgLink: "./img/all/Layer 26.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 29,
+    imgLink: "./img/all/Layer 27.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 30,
+    imgLink: "./img/all/Layer 28.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 31,
+    imgLink: "./img/all/Layer 29.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+  {
+    id: 32,
+    imgLink: "./img/all/Layer 30.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 33,
+    imgLink: "./img/all/Layer 31.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "wordpress",
+  },
+  {
+    id: 34,
+    imgLink: ".//img/all/Layer 32.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "landing-pages",
+  },
+  {
+    id: 35,
+    imgLink: "./img/all/Layer 33.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "web-design",
+  },
+  {
+    id: 36,
+    imgLink: "./img/all/Layer 34.png",
+    title: "Web Design",
+    alt: "Online Marketing",
+    category: "graphic-design",
+  },
+];
+
+const showImg = () => {
+  const workCards = document.querySelector(".work-cards");
+
+  let list = imagesArray.map((item, index) => {
+    if(index < step && index>=count){
+      const li = document.createElement("li");
+      li.classList.add("card");
+      li.classList.add("work-active");
+      li.dataset.category = item.category;
+
+      const img = new Image(290, 211);
+      img.classList.add("img");
+      img.src = item.imgLink;
+      img.alt = item.alt;
+
+      const infoCard = document.createElement("div");
+      infoCard.classList.add("info");
+
+      const infoRef = document.createElement("div");
+      infoRef.classList.add("info-ref");
+      infoRef.innerHTML =
+        "<a href='#'><span class='circle first-circ'>" +
+        "<img class='icon-link' alt='link' src='./img/icon/Combined shape 7431.png' width='14' height='14'/>" +
+        "</span></a>" +
+        "<a href='#'><span class='circle second-circ'>" +
+        "<img class='icon-link' alt='square' src='./img/icon/Layer 23.png' width='12' height='12'/>" +
+        "</span></a>";
+
+      const infoDescr = document.createElement("div");
+      infoDescr.classList.add("info-decri");
+      infoDescr.innerHTML = `<h3 class="info-title">creative design</h3><p class="info-paragraph">${item.title}</p>`;
+
+      infoCard.append(infoRef, infoDescr);
+      li.append(img, infoCard);
+
+      return li;
+    } return "";
+
+
+  });
+
+  workCards.append(...list);
+
+  if(step===imagesArray.length){
+    btnLoad.remove();
+  }
+};
+let step=12;
+let count=0;
+showImg();
+
+const btnLoad=document.querySelector(".btn-load");
+btnLoad.addEventListener('click', ()=>{
+  count+=12;
+  step+=12;
+  if(step <= imagesArray.length && count<=imagesArray.length){
+    showImg(step, count);
+  }else{
+    btnLoad.remove();
+  }
+})
+
+const workTitles = document.querySelectorAll(".work-tabs-title"),
+  
+  workTab = document.querySelector(".work-filter"),
+  activeCardClass = "work-active";
+
+workTab.addEventListener("click", (e) => {
+  const target = e.target.closest("li");
+  const workCards = document.querySelectorAll(".card");
+  selectTitle(target, workTitles, activeCardClass);
+  if (target.dataset.category !== "all") {
+    selectContent(target.dataset.category, workCards, activeCardClass);
+  } else {
+    workCards.forEach((item) => item.classList.add("work-active"));
+  }
 });
